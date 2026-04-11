d5f8ff5372285e550da8afc2a6b6086eaf23b9eb
TomRiddle67 <courage0047@gmail.com>
2026-04-11 12:43:37 +0100

    math functions

--- fundamentals/data-types.py
+++ fundamentals/data-types.py
@@ -1,8 +1,13 @@
 # int and float
 
-print(type(6+6))
-print(type(10-5))
-print(type(7*7))
-print(type(20/5))
-print(type(8%5))
-print(16%4)
+# print(type(6+6))
+# print(type(10-5))
+# print(type(7*7))
+# print(type(20/5))
+# print(type(8%5))
+# print(16%4)
+
+# math functions
+
+print(round(3.1))
+print (abs(-20))
