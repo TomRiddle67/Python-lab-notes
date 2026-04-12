b8cb08d4f27de774ee9f606fe9b2b8785ee26294
TomRiddle67 <courage0047@gmail.com>
2026-04-12 10:01:45 +0100

    password checker exercise

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -1,11 +1,17 @@
-user_age = 40
-user_name = "Tom"
+# user_age = 40
+# user_name = "Tom"
 
-man, is_from, works_at = "Tom", "London", "KFC"
+# man, is_from, works_at = "Tom", "London", "KFC"
 
-# print(user_name, user_age)
-print(man, is_from, works_at)
+# # print(user_name, user_age)
+# print(man, is_from, works_at)
 
-#statements and expressions
-kids_age = (user_age // 2)
-print (kids_age)
+# #statements and expressions
+# kids_age = (user_age // 2)
+# print (kids_age)
+
+username = input("username: ")
+password = input("password: ")
+user_password = password.replace( password[2], "****")
+
+print(f"welcome {username} your password is {user_password} ")
