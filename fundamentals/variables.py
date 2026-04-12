dbc1f5991b551f5de55110e7ee1f1aa8dc24f601
TomRiddle67 <courage0047@gmail.com>
2026-04-12 06:54:51 +0100

    Variables

---
+++ fundamentals/variables.py
@@ -0,0 +1,11 @@
+user_age = 40
+user_name = "Tom"
+
+man, is_from, works_at = "Tom", "London", "KFC"
+
+# print(user_name, user_age)
+print(man, is_from, works_at)
+
+#statements and expressions
+kids_age = (user_age // 2)
+print (kids_age)
