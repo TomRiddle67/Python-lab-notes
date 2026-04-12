b8fd1ac9928f64bd8d575676c06c74861e7ecfb5
TomRiddle67 <courage0047@gmail.com>
2026-04-12 06:48:51 +0100

    data types labs

--- fundamentals/data-types.py
+++ fundamentals/data-types.py
@@ -14,7 +14,44 @@
 
 #bin() and complex
 # this print out the binary reprensentation of 5
-print (bin(5))
-print (int("0b101", 2))
+# print (bin(5))
+# print (int("0b101", 2))
 
+#strings
+# first_name = "Tom"
+# last_name = "Riddle"
+# face = '''
+#     0 0
+#      -
+# '''
 
+# print(first_name, last_name, face)
+
+# string concatenation
+# print ("Hi" + " " +"there")
+
+#Type conversion
+# print (type (int(str(100))))
+# a = str (100)
+# b = int(a)
+# c = type (a), type(b)
+# print (c)
+
+#Escape sequences
+
+# weather = " it's 'kind of' sunny today \n \thope you have a good one"
+
+# # print (weather)
+# print (f'Hey 🖐 {weather}')
+
+#string indexes
+
+selfish = "01234567"
+
+print(selfish[::-1])
+print(selfish[1:2:])
+print(selfish[0])
+
+greet = "Hey 🖐"
+
+print(len(greet))
