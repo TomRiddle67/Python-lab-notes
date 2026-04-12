1bc63ec73aba7696db9bf9f166eef09040829064
TomRiddle67 <courage0047@gmail.com>
2026-04-12 06:53:20 +0100

    functions labs

---
+++ fundamentals/functions.py
@@ -0,0 +1,12 @@
+#methods
+# quote = "i think therefore i am"
+# print(quote.upper())
+# print(quote.replace("i am", "i become"))
+# print(quote.find("am"))
+# print(len(quote))
+
+
+birth_year = input('''what year were you born?
+''')
+age = 2026 - int(birth_year)
+print(f"You are {age} years old")
