54d9b620422865c8a1e74f0ef37e36b9aa6feb6d
TomRiddle67 <courage0047@gmail.com>
2026-04-13 17:10:24 +0100

    simple shopping cart

--- fundamentals/exercise.py
+++ fundamentals/exercise.py
@@ -1,9 +1,37 @@
-#if else statement
-is_friend = False
-message = 'chat with friend'
-not_friend = 'you are not friends'
+# cart = [{
+#     'name': 'shoes',
+#     'price': 80,
+#     'quantity': 1
+# },
+# {   'name': 'jacket',
+#     'price': 50,
+#     'quantity': 2
+# }
+# ]
+# total = 100
+# cart_total = cart[1].get('price') * cart[1].get('quantity') + cart[0].get('price') * cart[0].get('quantity')
+# if cart_total > total:
+#     print('You have a discount')
+# else:
+#     print('cart not full') #my code  😂🤦‍♂️
 
-if is_friend:
-    print(message)
+#Cleaner version
+
+cart = [{
+    'name': 'shoes',
+    'price': 80,
+    'quantity': 1
+},
+{   'name': 'jacket',
+    'price': 50,
+    'quantity': 2
+}]
+
+total = 0
+
+for item in cart:
+    total+= item['price'] * item['quantity']
+if total > 100:
+    print ('You have a discount')
 else:
-    print(not_friend)
+    print('cart not full')
