0bcfc123bce251649d2b828cd0ae73d3acf4ad48
TomRiddle67 <courage0047@gmail.com>
2026-04-16 10:10:11 +0100

    feat: initial data types and math operator challenge

--- fundamentals/exercise.py
+++ fundamentals/exercise.py
@@ -1,22 +1,10 @@
-account = {
-    "name": "Tom",
-    "balance": 100,
-    "is_active": True
-}
-def bank_action(account,action,amount=0):
-    if not account['is_active']:
-        return 'Account is inactive'
-    if action == 'balance':
-        return account['balance']
-    if action == 'deposit':
-        account['balance'] += amount
-        return amount['balance']
-    if action == 'withdraw':
-        if amount > account['balance']:
-            return 'insufficent funds'
-        account['balance'] -= amount
-        return account['balance']
-    return 'invalid action'
-print(bank_action(account, 'withdraw', amount=80))
-        
-    
+def calculator(a:int, b:float):
+    calculate_input = {
+        'sum': a + b,
+        'difference': a-b,
+        'product': a * b,
+        'division': a / b
+    }
+    return calculate_input
+result = calculator(10, 7.1)
+print(result)
