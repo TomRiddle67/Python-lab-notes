7b8bc5344bbb597cc3fcaea538e7977d494bee9c
TomRiddle67 <courage0047@gmail.com>
2026-04-16 10:37:00 +0100

    refactor: initial data types and math operator challenge

--- fundamentals/exercise.py
+++ fundamentals/exercise.py
@@ -1,10 +1,21 @@
-def calculator(a:int, b:float):
+
+def calculator(a:int, b:float) -> dict[str, float]:
+    '''
+     """
+Performs basic arithmetic operations on two numbers.
+
+Returns a dictionary containing:
+- sum
+- difference
+- product
+- division
+"""
+    '''
     calculate_input = {
         'sum': a + b,
-        'difference': a-b,
+        'difference': a - b,
         'product': a * b,
         'division': a / b
     }
     return calculate_input
 result = calculator(10, 7.1)
-print(result)
