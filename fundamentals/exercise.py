d5c4d698f12ef3d0f2dfd59fe89835fc63b86c38
TomRiddle67 <courage0047@gmail.com>
2026-04-16 15:20:29 +0100

    feat: add summary, f string logic

--- fundamentals/exercise.py
+++ fundamentals/exercise.py
@@ -1,22 +1,51 @@
 global_multiplier = 2
 
-def calculator(a:int, b:float) -> dict[str, float]:
+def calculator(a:int, b:float, name: str) -> dict[str, float | str]:
     """
-    Performs basic arithmetic operations on two numbers.
+    Performs arithmetic operations, scales numeric results,
+    and generates a personalized summary.
 
-    Returns a dictionary containing scaled results
+    Why this exists:
+    Combines computation with a user-friendly summary while keeping
+    logic clean and reusable.
+
+    Args:
+        a (int): First number
+        b (float): Second number
+        name (str): User's name
+
+    Returns:
+        dict[str, float | str]: Scaled results and a formatted summary
+
     """
+    sum_val = a + b
+    difference_val = a - b
+    product_val = a * b
+    division_val = a / b
+    
 
     calculate_output = {
-        'sum': a + b,
-        'difference': a - b,
-        'product': a * b,
-        'division': a / b
+        'sum': sum_val,
+        'difference': difference_val,
+        'product': product_val,
+        'division': division_val
     }
 
     scaled_output = {
-        key: value * global_multiplier
+        key: value * global_multiplier 
         for key, value in calculate_output.items()
     }
+    capitalized_name = name.capitalize()
+    first_letter = name[0]
+    name_length = len(name)
+
+    summary = (f"hello {capitalized_name}! {first_letter}"
+              f"Your name has {name_length} letters."
+              f"sum is {scaled_output['sum']}, product is {scaled_output['product']}")
+
+    scaled_output['summary'] = summary
+
     return scaled_output
-result = calculator(20, 2.0)
+
+result = calculator(20, 2.0, 'Tom Riddle')
+print(result)
